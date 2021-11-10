3a274bc2b99d0781c974d9d18fc3041f7ebeb1cb
kreyson999 <mlynekjakub05@gmail.com>
2021-11-11 00:23:59 +0100

    added dropdown menus

---
+++ assets/js/navbar.js
@@ -0,0 +1,17 @@
+// const nav = document.querySelector('nav');
+
+// const navOptions = {
+//     threshold: 1,
+// }
+// const navObserver = new IntersectionObserver(changeNav, navOptions)
+
+// function changeNav(entries, observer) {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             nav.classList.add('active');
+//         } else {
+//             nav.classList.remove('active');
+//         }
+//     });
+// }
+// navObserver.observe(observedSection);
