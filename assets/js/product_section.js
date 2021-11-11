7b4a81a01afbc6512a8d31520f468703fcad752c
kreyson999 <mlynekjakub05@gmail.com>
2021-11-11 18:33:54 +0100

    added scrolling products

--- assets/js/product_section.js
+++ assets/js/product_section.js
@@ -3,5 +3,18 @@ const productSections = document.querySelectorAll('.product-carousel');
 productSections.forEach(scroll);
 
 function scroll(section) {
-    
+    const carousel = section.querySelector('.container');
+    const productWidth = section.querySelector('.product-card').clientWidth;
+    const btnLeft = section.querySelector('.btn-left');
+    const btnRight = section.querySelector('.btn-right');
+    btnLeft.addEventListener('click', () => {
+        console.log(productWidth);
+        carousel.scrollLeft -= productWidth;
+        console.log(carousel.scrollLeft);
+    });
+    btnRight.addEventListener('click', () => {
+        console.log(productWidth);
+        carousel.scrollLeft += productWidth;
+        console.log(carousel.scrollLeft);
+    });
 }
