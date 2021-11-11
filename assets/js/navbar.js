6ad0ccb8d3acab1556f505151c43e18d3f6fd533
kreyson999 <mlynekjakub05@gmail.com>
2021-11-11 16:29:40 +0100

    navbar is now fixed

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,17 +1,21 @@
-// const nav = document.querySelector('nav');
+const nav = document.querySelector('nav');
+const header = document.querySelector('header');
+const navHeight = nav.clientHeight;
 
-// const navOptions = {
-//     threshold: 1,
-// }
-// const navObserver = new IntersectionObserver(changeNav, navOptions)
+const navOptions = {
+    threshold: 0,
+}
+const navObserver = new IntersectionObserver(changeNav, navOptions)
 
-// function changeNav(entries, observer) {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             nav.classList.add('active');
-//         } else {
-//             nav.classList.remove('active');
-//         }
-//     });
-// }
-// navObserver.observe(observedSection);
+function changeNav(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            header.style.paddingTop = `${navHeight}px`;
+            nav.classList.add('active');
+        } else {
+            header.style.paddingTop = `0px`;
+            nav.classList.remove('active');
+        }
+    });
+}
+navObserver.observe(header);
---
+++ assets/js/product_section.js
@@ -0,0 +1,7 @@
+const productSections = document.querySelectorAll('.product-carousel');
+
+productSections.forEach(scroll);
+
+function scroll(section) {
+    
+}
